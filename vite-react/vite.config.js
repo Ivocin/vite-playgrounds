1786d2a55cbba45f18e614bbf6e896d45d3c3ed2
清秋 <1044514593@qq.com>
2021-02-25 20:06:08 +0800

    feat: 配置 proxy

--- vite-react/vite.config.js
+++ vite-react/vite.config.js
@@ -22,4 +22,13 @@ export default defineConfig({
       },
     },
   },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://jsonplaceholder.typicode.com/",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 });
