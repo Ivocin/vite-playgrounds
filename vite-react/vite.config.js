7102e1ee39db26f7acb384d2117c19eda8354a39
清秋 <1044514593@qq.com>
2021-02-25 18:00:21 +0800

    feat: change antd theme to dark mode

--- vite-react/vite.config.js
+++ vite-react/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react-refresh";
+import { getThemeVariables } from "antd/dist/theme";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,9 +10,14 @@ export default defineConfig({
       less: {
         javascriptEnabled: true,
         modifyVars: {
-          "primary-color": "#1DA57A",
-          "link-color": "#1DA57A",
-          "border-radius-base": "2px",
+          ...getThemeVariables({
+            dark: true,
+          }),
+          ...{
+            "primary-color": "#1DA57A",
+            "link-color": "#1DA57A",
+            "border-radius-base": "2px",
+          },
         },
       },
     },
